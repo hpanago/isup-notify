9727acfd31b66dc1232fa07f4f63d7cf8d661af9
hpanago <hpanago@gmail.com>
2014-11-21 01:49:18 +0200

    The example.conf file

---
+++ example.conf.py
@@ -0,0 +1,16 @@
+
+#domains to check
+domains = ( 'google.gr', 'facebook.com', 'hpanago.com')
+
+#receiver(s)
+mail = 'foo@bar.com' 
+#if you have multiple emails
+#mail = ('foo@bar.com', 'foo2@bar.com')
+
+#sender email
+address = 'isup@bar.com'
+
+#Create a sendgrid account here: 
+#https://sendgrid.com/user/signup	
+sendgrid_username = 'foobar_API'
+sendgrid_password = 'foobar'
